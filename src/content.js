82b9371820b321a15916832a910e36146d5a2584
neko3cs <minaton.0722.net@gmail.com>
2021-10-25 16:25:04 +0900

    動くものが出来た

--- src/content.js
+++ src/content.js
@@ -1,15 +1,32 @@
-const confirmTweet = () => {
-  return confirm('投稿します。よろしいですか？');
+const confirmTweet = (e) => {
+  if (confirm('投稿します。よろしいですか？') === false) {
+    e.stopPropagation();
+  }
 };
 
-const onLoad = () => {
+const setAlertToTweetButton = () => {
   const tweetButton = document.querySelector('div[data-testid="tweetButtonInline"]');
   if (tweetButton) {
     console.log('ボタン見つけた！ダイアログを埋め込むね！');
-    tweetButton.addEventListener('click', confirmTweet);
+    tweetButton.addEventListener('click', confirmTweet, { capture: true });
   } else {
     console.log('ボタン見つからなかった！');
   }
+};
+
+const setAlertToAlertButton = () => {
+  const alertButton = document.querySelector('button#alertButton');
+  if (alertButton) {
+    console.log('ボタン見つけた！ダイアログを埋め込むね！');
+    alertButton.addEventListener('click', confirmTweet, { capture: true });
+  } else {
+    console.log('ボタン見つからなかった！');
+  }
+};
+
+const onLoad = () => {
+  setAlertToTweetButton();
+  // setAlertToAlertButton();
 };
 
 window.addEventListener('load', onLoad);
