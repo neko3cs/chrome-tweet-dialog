432d35dc5ead112f6f68ab4768edcb7134ac4347
neko3cs <minaton.0722.net@gmail.com>
2021-11-01 18:44:22 +0900

    refactor

--- src/content.js
+++ src/content.js
@@ -2,20 +2,18 @@ const confirmTweet = (e) => {
   if (confirm('投稿します。よろしいですか？') === false) {
     e.stopPropagation();
   }
-  setDialogToTweetButton();  // TODO: 何故か再設定されない->直す
 };
 
-const setDialogToTweetButton = () => {
-  // TODO: なんか見つからない時があるので取り方を考える
+const setConfirmToTweetButton = () => {
   const tweetButton = document.querySelector('div[data-testid="tweetButtonInline"]');
   if (tweetButton === null) {
     throw ('Can not get tweet button element!');
   }
-  tweetButton.addEventListener('click', confirmTweet, { capture: true });
+  tweetButton.addEventListener('click', confirmTweet, {
+    once: false,
+    passive: true,
+    capture: true
+  });
 };
 
-const onLoad = () => {
-  setDialogToTweetButton();
-};
-
-window.addEventListener('load', onLoad);
+window.addEventListener('load', setConfirmToTweetButton);
