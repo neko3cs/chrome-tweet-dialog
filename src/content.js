c61cb8b049382d475fc97ba41d734a2413558654
neko3cs <minaton.0722.net@gmail.com>
2022-03-22 00:10:20 +0900

    【WIP】ctrl+enterの対応

--- src/content.js
+++ src/content.js
@@ -22,4 +22,25 @@ const setConfirmToTweetButton = () => {
   setIntervalId = setInterval(innerMethod, 1000);
 };
 
+// FIXME: うまくCtrl+Enterを補足出来ていない
+const setConfirmToCtrlEnter = () => {
+  let setIntervalId = 0;
+
+  const innerMethod = () => {
+    const tweetTextArea = document.querySelector('div[data-testid="tweetTextarea_0"]');
+    if (tweetTextArea === null) {
+      return;
+    }
+    tweetTextArea.addEventListener('keypress', (event) => {
+      if (event.keyCode === 13 && event.ctrlKey === true) {
+        confirmTweet();
+      }
+    }, { capture: true });
+    clearInterval(setIntervalId);
+  };
+
+  setIntervalId = setInterval(innerMethod, 1000);
+}
+
 window.addEventListener('load', setConfirmToTweetButton);
+window.addEventListener('load', setConfirmToCtrlEnter);
